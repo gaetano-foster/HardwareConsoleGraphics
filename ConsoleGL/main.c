18d97c8f8a6889634aa36bb05ec44e5e0281c26c
Gaetano Foster <gaetanox109@gmail.com>
2026-07-30 13:52:45 -0400

    Implemented: basic first person controller

--- ConsoleGL/main.c
+++ ConsoleGL/main.c
@@ -11,6 +11,38 @@
 #include "object.h"
 #include "camera.h"
 
+#define SPEED		(0.05f)
+#define SENS		(0.05f)
+
+struct {
+	BOOL W, A, S, D, SPACE, LSHIFT, UP, DOWN, LEFT, RIGHT;
+} keys;
+
+void
+move_camera()
+{
+	if (keys.W)
+		camera_translatel((vec3) { 0, 0, SPEED });
+	if (keys.S)
+		camera_translatel((vec3) { 0, 0, -SPEED });
+	if (keys.A)
+		camera_translatel((vec3) { -SPEED, 0, 0 });
+	if (keys.D)
+		camera_translatel((vec3) { SPEED, 0, 0 });
+	if (keys.SPACE)
+		camera_translatel((vec3) { 0, SPEED, 0 });
+	if (keys.LSHIFT)
+		camera_translatel((vec3) { 0, -SPEED, 0 });
+	if (keys.UP)
+		camera_rotate_rad((vec3) { SENS, 0, 0 });
+	if (keys.DOWN)
+		camera_rotate_rad((vec3) { -SENS, 0, 0 });
+	if (keys.LEFT)
+		camera_rotate_rad((vec3) { 0, -SENS, 0 });
+	if (keys.RIGHT)
+		camera_rotate_rad((vec3) { 0, SENS, 0 });
+}
+
 int
 main(int argc,
 	 char **argv)
@@ -45,9 +77,9 @@ main(int argc,
 	object_setpos(&triangle, (vec3) { 0.0f, 0.0f, -1.0f });
 
 	struct camera_config_t config = {
-		.x = -2,
+		.x = -0.5,
 		.y = 0,
-		.z = 0,
+		.z = -0.5,
 		.pitch = 0,
 		.yaw = 0,
 		.roll = 0,
@@ -60,13 +92,83 @@ main(int argc,
 
 	while (running) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+			switch (event.type) {
+			case SDL_QUIT: 
 				running = FALSE;
+				break;
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_W:
+					keys.W = TRUE;
+					break;
+				case SDL_SCANCODE_S:
+					keys.S = TRUE;
+					break;
+				case SDL_SCANCODE_A:
+					keys.A = TRUE;
+					break;
+				case SDL_SCANCODE_D:
+					keys.D = TRUE;
+					break;
+				case SDL_SCANCODE_SPACE:
+					keys.SPACE = TRUE;
+					break;
+				case SDL_SCANCODE_LSHIFT:
+					keys.LSHIFT = TRUE;
+					break;
+				case SDL_SCANCODE_UP:
+					keys.UP = TRUE;
+					break;
+				case SDL_SCANCODE_DOWN:
+					keys.DOWN = TRUE;
+					break;
+				case SDL_SCANCODE_LEFT:
+					keys.LEFT = TRUE;
+					break;
+				case SDL_SCANCODE_RIGHT:
+					keys.RIGHT = TRUE;
+					break;
+				}
+				break;
+			case SDL_KEYUP:
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_W:
+					keys.W = FALSE;
+					break;
+				case SDL_SCANCODE_S:
+					keys.S = FALSE;
+					break;
+				case SDL_SCANCODE_A:
+					keys.A = FALSE;
+					break;
+				case SDL_SCANCODE_D:
+					keys.D = FALSE;
+					break;
+				case SDL_SCANCODE_SPACE:
+					keys.SPACE = FALSE;
+					break;
+				case SDL_SCANCODE_LSHIFT:
+					keys.LSHIFT = FALSE;
+					break;
+				case SDL_SCANCODE_UP:
+					keys.UP = FALSE;
+					break;
+				case SDL_SCANCODE_DOWN:
+					keys.DOWN = FALSE;
+					break;
+				case SDL_SCANCODE_LEFT:
+					keys.LEFT = FALSE;
+					break;
+				case SDL_SCANCODE_RIGHT:
+					keys.RIGHT = FALSE;
+					break;
+				}
+				break;
 			}
 		}
+		move_camera();
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		object_rotate(&triangle, 0.1f, (vec3) { 0.0f, 1.0f, 0.0f });
 		object_draw(&triangle);
 		conscr_render();
 		context_swap();
