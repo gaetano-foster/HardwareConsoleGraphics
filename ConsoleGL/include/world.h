bfb8819be5d4312e6eda111278f3268b3db3f3ae
Gaetano Foster <gaetanox109@gmail.com>
2026-08-10 17:07:10 -0400

    Created world module, hash map of chunks

---
+++ ConsoleGL/include/world.h
@@ -0,0 +1,35 @@
+#ifndef __WORLD_H__
+#define __WORLD_H__
+#include "chunk.h"
+
+struct cnode_t {
+	int x;
+	int z;
+	chunk_t *chunk;
+	struct cnode_t *next;
+};
+
+typedef struct _world_t {
+	size_t csize;
+	size_t ccapacity;
+	struct cnode_t **chunks;
+} world_t;
+
+world_t *
+world_init();
+
+void
+world_cleanup(world_t *world);
+
+chunk_t *
+world_chunk(world_t *world,
+	int x_offset, int z_offset);
+
+chunk_t *
+world_genchunk(world_t *world,
+	int x_offset, int z_offset);
+
+void
+world_render(world_t *world);
+
+#endif
---
+++ ConsoleGL/src/world.c
@@ -0,0 +1,125 @@
+#include <Windows.h>
+#include "utils.h"
+#include "world.h"
+
+////
+/// Hash map functions
+//
+
+static DWORD
+hash(int x, int y) 
+{
+	DWORD h = 17;
+	h = h * 31 + (DWORD)x;
+	h = h * 31 + (DWORD)y;
+	return h;
+}
+
+static void
+world_rehash(world_t *world)
+{
+	int old_capacity = world->ccapacity;
+	world->ccapacity *= 2;
+	struct cnode_t **new_chunks =
+		calloc(world->ccapacity, sizeof(struct cnode_t *));
+	EXPECT(new_chunks);
+
+	for (int i = 0; i < old_capacity; i++) {
+		struct cnode_t *entry = world->chunks[i];
+		while (entry) {
+			struct cnode_t *next = entry->next;
+			unsigned int slot =
+				hash(entry->x, entry->z) % world->ccapacity;
+			entry->next = new_chunks[slot];
+			new_chunks[slot] = entry;
+			entry = next;
+		}
+	}
+	free(world->chunks);
+	world->chunks = new_chunks;
+}
+
+////
+/// World functions
+//
+
+world_t *
+world_init()
+{
+	world_t *world = malloc(sizeof(world_t));
+	EXPECT(world);
+	world->ccapacity = 100;
+	world->csize = 0;
+	world->chunks = calloc(world->ccapacity, sizeof(struct cnode_t *));
+	EXPECT(world->chunks);
+	return world;
+}
+
+void
+world_cleanup(world_t *world)
+{
+	for (int i = 0; i < world->ccapacity; i++) {
+		struct cnode_t *entry = world->chunks[i];
+		while (entry != NULL) {
+			struct cnode_t* temp = entry;
+			entry = entry->next;
+			chunk_cleanup(temp->chunk);
+			free(temp); 
+		}
+	}
+	free(world);
+}
+
+chunk_t *
+world_chunk(world_t *world,
+	int x_offset, int z_offset)
+{
+	unsigned int slot = hash(x_offset, z_offset) % world->ccapacity;
+	struct cnode_t* entry = world->chunks[slot];
+
+	while (entry != NULL) {
+		if (entry->x == x_offset && entry->z == z_offset) {
+			return entry->chunk;
+		}
+		entry = entry->next;
+	}
+	return world_genchunk(world, x_offset, z_offset); 
+}
+
+chunk_t *
+world_genchunk(world_t *world,
+	int x_offset, int z_offset)
+{
+	if (world->csize >= world->ccapacity * 0.75) {
+		world_rehash(world);
+	}
+	// allocate chunk
+	chunk_t *chunk = chunk_init((vec2) { x_offset, z_offset });
+	EXPECT(chunk);
+	// allocate node
+	struct cnode_t *node = malloc(sizeof(struct cnode_t));
+	EXPECT(node);
+	// populate node
+	node->x = x_offset;
+	node->z = z_offset;
+	node->chunk = chunk;
+
+	unsigned int slot = hash(x_offset, z_offset) % world->ccapacity;
+	node->next = world->chunks[slot];
+	world->chunks[slot] = node;
+	world->csize++;
+	return chunk;
+}
+
+void
+world_render(world_t *world)
+{
+	for (int i = 0; i < world->ccapacity; i++) {
+		struct cnode_t *entry = world->chunks[i];
+
+		while (entry) {
+			chunk_render(entry->chunk);
+			entry = entry->next;
+		}
+	}
+}
